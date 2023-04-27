4d1f710e3105249efec769c2feafaaad63cf6f36
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2023-04-27 21:43:52 +0200

    Added nupkg, Version 3.0.1 - namespace has to be modified

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+[assembly: AssemblyTitle("Data Access Layer ShareBucket")]
+[assembly: AssemblyDescription("Data Access Layer for Sharebucket project.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ShareBucket")]
+[assembly: AssemblyProduct("Data Access Layer ShareBucket")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("5402b32f-1b7d-4022-912b-47918a998b4c")]
+
+
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
